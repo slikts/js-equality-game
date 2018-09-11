828e395fe2fe01c0e2f441f1f6d7740b6742b65e
slikts <dabas@untu.ms>
2018-09-11 15:41:01 +0300

    add polyfills

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom"
 import "./css/index.css"
 import App from "./components/App"
 import registerServiceWorker from "./registerServiceWorker"
+import "@babel/polyfill"
 
 ReactDOM.render(<App />, document.getElementById(`root`))
 registerServiceWorker()
