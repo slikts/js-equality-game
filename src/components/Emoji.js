503d733b1268cf018214449b905388325dadc4f3
slikts <dabas@untu.ms>
2018-09-16 22:25:47 +0300

    fix preload

--- src/components/Emoji.js
+++ src/components/Emoji.js
@@ -1,17 +1,8 @@
 import * as React from "react"
-import * as emojiSupport from "detect-emoji-support"
 import "../css/Emoji.css"
-
-const preload = new Set()
-const emojis = emojiSupport()
 
 const Emoji = ({ symbol, label }) => {
   const path = `./emojis/${symbol.codePointAt(0).toString(16)}.svg`
-  if (emojis && !preload.has(path)) {
-    const image = new Image()
-    image.src = path
-    preload.add(path)
-  }
   return (
     <span
       className={`Emoji Emoji-${label}`}
