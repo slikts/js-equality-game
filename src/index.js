3ba5adf44e0b356007acf3f5150ad23e698fd238
slikts <dabas@untu.ms>
2018-12-09 18:36:09 +0200

    remove service worker

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./css/index.css"
 import App from "./components/App"
-import registerServiceWorker from "./registerServiceWorker"
 import "@babel/polyfill"
 import "whatwg-fetch"
 
 ReactDOM.render(<App />, document.getElementById(`root`))
-registerServiceWorker()
 
 if (!emojiSupport()) {
   fetch(`./emojis/files.json`)
