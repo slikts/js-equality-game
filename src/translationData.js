bbbfb429b5567fb8517f5c4c8388980580e4e439
slikts <dabas@untu.ms>
2018-12-10 15:26:30 +0200

    add english

--- src/translationData.js
+++ src/translationData.js
@@ -1,6 +1,8 @@
 import latvian from "./translations/lv-LV.json"
+import english from "./translations/en-US.json"
 
 const translationData = new Map() //
   .set(`lv-LV`, latvian)
+  .set(`en-US`, english)
 
 export default translationData
