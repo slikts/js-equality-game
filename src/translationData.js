5ca8c6911662f0892397074238963ebb11d3393c
slikts <dabas@untu.ms>
2018-12-10 17:05:15 +0200

    enable nl-NL

--- src/translationData.js
+++ src/translationData.js
@@ -1,8 +1,10 @@
 import latvian from "./translations/lv-LV.json"
 import english from "./translations/en-US.json"
+import dutch from "./translations/nl-NL.json"
 
 const translationData = new Map() //
   .set(`en-US`, { data: english, name: `English` })
+  .set(`nl-NL`, { data: dutch, name: `Dutch` })
   .set(`lv-LV`, { data: latvian, name: `Latvian` })
 
 export default translationData
