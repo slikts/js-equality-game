586ddd05b6557adbbbe1ae5c6ad6e9ee64f3acba
slikts <dabas@untu.ms>
2018-12-16 16:30:46 +0200

    fix

--- src/translationData.js
+++ src/translationData.js
@@ -2,6 +2,7 @@ import latvian from "./translations/lv-LV.json"
 import english from "./translations/en-US.json"
 import dutch from "./translations/nl-NL.json"
 import french from "./translations/fr-FR.json"
+import german from "./translations/de-DE.json"
 
 const translationData = new Map() //
   .set(`en-US`, { data: english, name: `English` })
