3797b6449b7eb11456247020f117766e2d3b24e6
stenno <stenno@stennos-mbp.speedport_w_723v_1_46_000>
2018-12-16 15:22:01 +0100

    german translation

--- src/translationData.js
+++ src/translationData.js
@@ -2,11 +2,13 @@ import latvian from "./translations/lv-LV.json"
 import english from "./translations/en-US.json"
 import dutch from "./translations/nl-NL.json"
 import french from "./translations/fr-FR.json"
+import german from "./translations/de-DE.json"
 
 const translationData = new Map() //
   .set(`en-US`, { data: english, name: `English` })
   .set(`fr-FR`, { data: french, name: `Français` })
   .set(`nl-NL`, { data: dutch, name: `Nederlands` })
   .set(`lv-LV`, { data: latvian, name: `Latviešu` })
+  .set(`de-DE`, { data: german, name: `Deutsch` })
 
 export default translationData
