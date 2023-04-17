33436d3fb373f11000c8479f040862f7bb5a51d4
Artem Veremienko <temaspsxxx@gmail.com>
2023-04-18 00:05:33 +0300

    feat: add translation data

--- src/translationData.js
+++ src/translationData.js
@@ -1,12 +1,13 @@
-import latvian from "./translations/lv-LV.json"
-import english from "./translations/en-US.json"
-import dutch from "./translations/nl-NL.json"
-import french from "./translations/fr-FR.json"
-import german from "./translations/de-DE.json"
-import polish from "./translations/pl-PL.json"
-import spanish from "./translations/es-ES.json"
-import simplifiedChinese from "./translations/zh-CN.json"
-import korean from "./translations/ko-KR.json"
+import latvian from './translations/lv-LV.json'
+import english from './translations/en-US.json'
+import dutch from './translations/nl-NL.json'
+import french from './translations/fr-FR.json'
+import german from './translations/de-DE.json'
+import polish from './translations/pl-PL.json'
+import spanish from './translations/es-ES.json'
+import simplifiedChinese from './translations/zh-CN.json'
+import korean from './translations/ko-KR.json'
+import ukrainian from './translations/uk-UA.json'
 
 const translationData = new Map() //
   .set(`en-US`, { data: english, name: `English` })
@@ -18,5 +19,6 @@ const translationData = new Map() //
   .set(`es-ES`, { data: spanish, name: `Spanish` })
   .set(`zh-CN`, { data: simplifiedChinese, name: `简体中文` })
   .set(`ko-KR`, { data: korean, name: `한국어` })
+  .set(`uk-UA`, { data: ukrainian, name: `Українська` })
 
 export default translationData
